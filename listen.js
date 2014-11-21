0d5c3b35a61935fd7a3a236acb186c123e557917
Brian Underwood <ml+github@semi-sentient.com>
2014-11-21 21:51:42 +0100

    For consistency

--- listen.js
+++ listen.js
@@ -1,10 +1,10 @@
 var app = require('express')();
 
 var port = process.env.PORT || 5000;
-var API_PORT = port;
+var api_port = port;
 
-web_app = require('./web/app')(app, API_PORT)
-api_app = require('./api/app')(app, API_PORT)
+web_app = require('./web/app')(app, api_port)
+api_app = require('./api/app')(app, api_port)
 
 app.listen(port, function() {
   console.log("Listening on " + port);
