f6c1ea8c99dea2165a00b064f295fb9cf7fc491f
Brian Underwood <ml+github@semi-sentient.com>
2014-12-12 18:03:08 +0100

    Fix random error

--- web/dist/assets/js/application.js
+++ web/dist/assets/js/application.js
@@ -4,11 +4,13 @@ $(document).ready(function($) {
 
 	var owlCast = $("#owl-demo-cast");
 
-	owlCast.owlCarousel({
-		items : 6,
-		itemsDesktop : [1199,6],
-		itemsDesktopSmall : [980,4],
-		itemsTablet: [768,3],
-		itemsMobile: [479, 2]
-	});
+  if (owlCast.owlCarousel) {
+    owlCast.owlCarousel({
+      items : 6,
+      itemsDesktop : [1199,6],
+      itemsDesktopSmall : [980,4],
+      itemsTablet: [768,3],
+      itemsMobile: [479, 2]
+    });
+  }
 });
