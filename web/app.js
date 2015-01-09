8c5d0121415a1d3e9d0429d3d3eec7372ec558ee
Brian Underwood <ml+github@semi-sentient.com>
2015-01-09 16:56:27 +0200

    Don't forward in non-production

--- web/app.js
+++ web/app.js
@@ -14,16 +14,16 @@ module.exports = function (app, api_port) {
   app.locals.load_cache = { };
 
   app.all(/.*/, function(req, res, next) {
-//    if (process.env.NODE_ENV == 'production') {
+    if (process.env.NODE_ENV == 'production') {
       var host = req.header('host');
       if (host !== 'graphgist.neo4j.com') {
         res.redirect(301, 'http://graphgist.neo4j.com' + req.path);
       } else {
         next();
       }
-//    } else {
-//      next()
-//    }
+    } else {
+      next()
+    }
 
   });
 
