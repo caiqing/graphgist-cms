d268336ac4b25003d268b34a2eb762cbdd082cf4
Michael Hunger <github@jexp.de>
2015-07-30 11:58:07 +0200

    Added support for Neo4j 2.2, AsciiDoc attribute :env-grapghist:

--- web/dist/assets/js/graphgist.js
+++ web/dist/assets/js/graphgist.js
@@ -41,7 +41,7 @@ function GraphGist($, options) {
     var $VISUALIZATION_ICONS = $('<div class="visualization-icons"><i class="glyphicon glyphicon-fullscreen fullscreen-icon" title="Toggle fullscreen mode"></i></div>');
     var $I = $('<i/>');
 
-    var DEFAULT_VERSION = '2.1';
+    var DEFAULT_VERSION = '2.2';
     var CONSOLE_VERSIONS = { '2.0.0-M06': 'http://neo4j-console-20m06.herokuapp.com/',
         '2.0.0-RC1': 'http://neo4j-console-20rc1.herokuapp.com/',
         '2.0.0': 'http://neo4j-console-20.herokuapp.com/',
@@ -54,6 +54,8 @@ function GraphGist($, options) {
         '2.1.2': 'http://neo4j-console-21.herokuapp.com/',
         '2.1.3': 'http://neo4j-console-21.herokuapp.com/',
         '2.1': 'http://neo4j-console-21.herokuapp.com/',
+        '2.2': 'http://neo4j-console-22.herokuapp.com/',
+        '2.3': 'http://neo4j-console-23.herokuapp.com/',
         'local': 'http://localhost:8080/',
         '1.9': 'http://neo4j-console-19.herokuapp.com/'
     };
@@ -84,7 +86,7 @@ function GraphGist($, options) {
 
         if (options.preProcess) {
           console.log('Pre processing gist');
-          var asciidoctor_options = Opal.hash('attributes', [ 'notitle!' ], 'attribute-missing', 'drop');
+          var asciidoctor_options = Opal.hash('attributes', [ 'notitle!','env-graphgist' ], 'attribute-missing', 'drop');
           var content = preProcessContents(content);
           if (imagesdir) {
               // probably not supported yet due to a bug
